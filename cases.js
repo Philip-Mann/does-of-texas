dcc531283f2b58476ed7d227c863cac6aa7c5bd5
Philip Mann <mannphilip7@gmail.com>
2021-05-15 15:27:05 -0500

    initial commit

---
+++ cases.js
@@ -0,0 +1,246 @@
+module.exports = {
+    
+    "001": {
+            "doe_name": "Mr. X",
+            "id": "001",
+            "sex": "Male",
+            "age": "16 - 24",
+            "race": "White/Hispanic",
+            "height": "5'6",
+            "weight": "110lbs",
+            "location_found": "Johnson County",
+            "body_condition": "Decomposed",
+            "year_found": 1972,
+            "image": "https://dnadoeproject.org/wp-content/uploads/2020/10/MrXJohnsonCo1972_Koppelman_web.jpg",
+            "cod": "Gun Shot (Homicide)",
+            "info": "Mr. X is the nickname given to a murder victim discovered on a farm near Cleburne, Johnson County, Texas in 1972. The male's case was reopened in 2012, which has led to numerous adjustments to his physical traits. The motive for his murder was possibly out of retaliation.",
+            "link": "https://unidentified.wikia.org/wiki/Mr._X_(1972)",
+            "characteristics": [
+                {
+                    "one": "Short, curly brown hair cut not long before his murder."
+                }
+            ]
+        },
+
+    "002": {
+            "doe_name": "Swimsuit Boy",
+            "id": "002",
+            "sex": "Male",
+            "age": "15 - 19",
+            "race": "White/Hispanic",
+            "height": "5'2 - 5'7 ",
+            "weight": "Unknown",
+            "location_found": "Harris County",
+            "body_condition": "Skeletal",
+            "year_found": 1973, 
+            "image": "https://api.missingkids.org/photographs/NCMU1109009c1.jpg",
+            "cod": "Homicide",
+            "info": "Swimsuit Boy is the only unidentified victim of serial killer Dean Corll whose body was discovered in 1973. He most likely disappeared between 1971 and 1972. 13 people have been ruled out as being the decedent. He is currently undergoing testing by the DNA Doe Project. Their involvement with the case was put on an indefinite hold at the request of the investigating agency in August, 2020.",
+            "link": "https://unidentified.wikia.org/wiki/Swimsuit_Boy",
+            "characteristics": [
+                {
+                    "one": "Brown Hair.",
+                    "two": "Good dentition with no fillings."
+                }
+            ]
+        },
+
+    "003": {
+            "doe_name": "Disco Dan",
+            "id": "003",
+            "sex": "Male",
+            "age": "20 - 30",
+            "race": "White",
+            "height": "5'8",
+            "weight": "128lbs",
+            "location_found": "Hewitt, Tx",
+            "body_condition": "Decomposed",
+            "year_found": 1978,
+            "image": "http://www.doenetwork.org/cases/images/2885UMTX_LARGE.jpg",
+            "cod": "Gun Shot (Homicide)",
+            "info": "Disco Dan was an unidentified man found murdered in McLennan County, Texas in 1978. He was given his nickname due to pattern of the shirt found with him. The current location of Disco Dan's body remains unknown.",
+            "link": "https://unidentified.wikia.org/wiki/Disco_Dan",
+            "characteristics": [
+                {
+                    "one": "Light brown, shoulder-length hair.",
+                    "two": "Mustache and goatee.",
+                    "three": "Brown eyes.",
+                    "four": "A tattoo of a cross and heart on upper left arm.",
+                    "five": "A tattoo of Peace on the left forearm.",
+                    "six": " The initials RB tattoed on his upper right arm."
+                }
+            ]
+        },
+
+    "004": {
+            "doe_name": "Walker County Jane Doe",
+            "id": "004",
+            "sex": "Female",
+            "age": "14 - 18",
+            "race": "White",
+            "height": "5'6",
+            "weight": "105 - 120lbs",
+            "location_found": "Huntsville, Tx",
+            "body_condition": "Recognizable Face",
+            "year_found": 1980, 
+            "image": "https://upload.wikimedia.org/wikipedia/commons/d/d2/Walker_County_ID_photo_fx_Recon_007b.jpg",
+            "cod": "Ligature Strangulation (Homicide)",
+            "info": "Walker County Jane Doe was a teenager or young adult found murdered in Huntsville, Texas on November 1, 1980. She is believed to have been seen alive hours before her body was found, requesting directions to a local prison.",
+            "link": "https://unidentified.wikia.org/wiki/Walker_County_Jane_Doe",
+            "characteristics": [
+                {
+                    "one": "Light brown hair, in a wing style.",
+                    "two": "Hazel Eyes.",
+                    "three": "Painted toenails a light pink.",
+                    "four": "Pierced ears, however none recovered with body.",
+                    "five": "Healthy weight.",
+                    "six": "One or more dental fillings."
+                }
+            ]
+        },
+
+    "005": {
+            "doe_name": "Harris County Jane Doe",
+            "id": "005",
+            "sex": "Female",
+            "age": "20 -31",
+            "race": "Black/Asian/East Indian",
+            "height": "4'8 - 4'10",
+            "weight": "90 - 110lbs",
+            "location_found": "Harris County",
+            "body_condition": "Decomposed",
+            "year_found": 1980,
+            "image": "http://www.doenetwork.org/cases/images/477UFTX.jpg",
+            "cod": "Ligature Strangulation (Homicide)",
+            "info": "The female's body was discovered along I-45, near a barbed wire fence. Early into the investigation, the female had been theorized to have been one of various high risk victims, including prostitutes, who were killed and left along Texas highways in the late 1970s and 1980s.",
+            "link": "https://unidentified.wikia.org/wiki/Harris_County_Jane_Doe_(October_1980)",
+            "characteristics": [
+                {
+                    "one": "Black hair that was styled in various braids.",
+                    "two": "Possibly suffered from gum disease, as suggested by her lower set of teeth."
+                }
+            ]
+        },
+
+    "006": {
+            "doe_name": "Grimes County Jane Doe",
+            "id": "006",
+            "sex": "Female",
+            "age": "13 - 19",
+            "race": "White/Hispanic",
+            "height": "5'1 - 5'4",
+            "weight": "110lbs",
+            "location_found": "Grimes County",
+            "body_condition": "Decomposed",
+            "year_found": 1981, 
+            "image": "http://www.doenetwork.org/cases/images/543UFTX.jpg",
+            "cod": "Blunt Force Trauma (Homicide)",
+            "info": "A highway worker discovered the victim's body on October 29, 1981. Her cause of death was blunt force trauma to the head. Her body was wrapped in a plastic bag also containing a white rag, believed to be the type used in mechanical work. The remains may have been stored at a different location before being deposited at the location, possibly years after her death. She is possibly a victim of Henry Lee Lucas.",
+            "link": "https://unidentified.wikia.org/wiki/Grimes_County_Jane_Doe",
+            "characteristics": [
+                {
+                    "one": "Red or Auburn Hair.",
+                    "two": "Right handed.",
+                    "three": "Extensive dental care with 13 fillings."
+                }
+            ]
+        },
+
+    "007": {
+            "doe_name": "Harris County Jane Doe",
+            "id": "007",
+            "sex": "Female",
+            "age": "16 - 35",
+            "race": "Hispanic",
+            "height": "5 - 5'2",
+            "weight": "120 - 140lbs",
+            "location_found": "Harris County",
+            "body_condition": "Recognizable Face",
+            "year_found": 1985, 
+            "image": "http://www.doenetwork.org/cases/images/32UFTX2_LARGE.jpg",
+            "cod": "Homicide",
+            "info": "Harris County Jane Doe was a female found murdered in Texas in 1985. She was located inside of a closed business with a deceased man, identified as Thomas Rodriguez. Law enforcement is not sure how they were able to get inside where they were found.",
+            "link": "https://unidentified.wikia.org/wiki/Harris_County_Jane_Doe_(1985)",
+            "characteristics": [
+                {
+                    "one": "Long, dark brown hair.",
+                    "two": "Brown eyes.",
+                    "three": "Double pierced ears.",
+                    "four": "Scars on her left wrist and hand",
+                    "five": "A cholecystectomy scar approximately 5 inches in length.",
+                    "six": "The letter V tattooed on her upper left arm.",
+                    "seven": "She was missing four teeth for unspecified reasons but the rest of her teeth where in good condition."
+                }
+            ]
+        },
+
+    "008": {
+            "doe_name": "Bexar County Jane Doe",
+            "id": "008",
+            "sex": "Female",
+            "age": "18 - 30",
+            "race": "Black",
+            "height": "5'11 - 6'",
+            "weight": "150 - 160lbs",
+            "location_found": "Bexar County",
+            "body_condition": "Decomposed",
+            "year_found": 1986, 
+            "image": "http://www.doenetwork.org/cases/images/76UFTX.jpg",
+            "cod": "Gun Shot (Homicide)",
+            "info": "Norma was a woman found murdered in San Antonio, Texas in 1986. She was the victim of Ángel M. Reséndiz AKA the Railroad Killer.",
+            "link": "https://unidentified.wikia.org/wiki/Norma",
+            "characteristics": [
+                {
+                    "one": "Black hair",
+                    "two": "Possibly brown eyes.",
+                    "three": "Upper left canine tooth diplaced inward."
+                }
+            ]
+        },
+
+    "009": {
+            "doe_name": "IDENTIFIED: Live Oak Doe",
+            "id": "009",
+            "sex": "Possibly a Male transitioned into Female",
+            "age": "20 -40",
+            "race": "Withheld",
+            "height": "Withheld",
+            "weight": "Withheld",
+            "location_found": "Harris County",
+            "body_condition": "Recognizable",
+            "year_found": 1986,
+            "image": "http://www.doenetwork.org/cases/images/77umtx1.jpg",
+            "cod": "Cardiac Arrest by Ligature Strangulation (Homicide)",
+            "info": "Live Oak Doe was an individual located in Texas in 1986. The victim was assigned male at birth, yet was dressed in female clothing when discovered. They were identified in January 2021 by the DNA Doe Project. No further information is available at this time.",
+            "link": "https://unidentified.wikia.org/wiki/Live_Oak_Doe",
+            "characteristics": []
+        },
+
+    "010": {
+            "doe_name": "New York Billy",
+            "id": "010",
+            "sex": "Male",
+            "age": "20 - 35",
+            "race": "White",
+            "height": "5'5 - 5'7",
+            "weight": "130 - 140lbs",
+            "location_found": "Conroe,Tx",
+            "body_condition": "Decomposed",
+            "year_found": 1986,  
+            "image": "http://www.doenetwork.org/cases/images/84UMTX3_LARGE.jpg",
+            "cod": "Gun Shot (Homicide)",
+            "info": "On August 3, 1986, several persons in Crater Lake observed what appeared to be a body floating in the water. When authorities responded, they found the decomposed body of a young white male. He had been shot to death and had been dead for about a week. After the man was killed, it appeared that he was tied to two cinder blocks with an electrical cord and then deposited in the lake. He may have gone by the street name, New York Billy. He was exhumed in 2015 in order to analyze his DNA and make a forensic reconstruction.",
+            "link": "https://unidentified.wikia.org/wiki/New_York_Billy",
+            "characteristics": [
+                {
+                    "one": "A tattoo of a small devil with a painted tail of his lower left arm.",
+                    "two": "A tattoo of the word Liz on his upper left arm.",
+                    "three": "A tattoo of the words Baby Dawn and a flower type design on his right arm.",
+                    "four": "Reddish-brown shoulder length hair.",
+                    "five": "Pierced left ear.",
+                    "six": "Decayed teeth",
+                    "seven": "Some wisdom teeth had been surgically removed"
+                }
+            ]
+        }
+}
---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const cors = require('cors')
+const es6Renderer = require('express-es6-template-engine');
+const cases = require('./cases')
+
+const app = express();
+
+app.use(cors())     //cors express middleware
+app.use(express.static('public'))     //middleware for static files
+
+app.engine('html', es6Renderer);     
+app.set('views', 'templates');       
+app.set('view engine', 'html');
+
+const name = "Unidentified Does of Texas"
+const qouter = "There are 1,621 unidentified deceased in the state of Texas as of September 10th 2019"
+const linked = ' \"https://www.nbcdfw.com/news/local/fort-worth-home-to-database-for-missing-unidentified-dead/1964622/#:~:text=There%20were%201%2C621%20unidentified%20bodies,and%2046%20in%20Fort%20Worth.\" '
+const newsSource = "NBC DFW"
+
+
+app.get('/', (req, res) => {
+
+    const caseIds = Object.keys(cases)      //takes an object and
+    const newCaseArray = caseIds.map( id => cases[id])      //converts it into an array
+    const newImageArray = caseIds.map( image => cases[image])
+
+    res.render('home', {
+        locals: {       //makes an object
+            title: `${name}`,       //makes another object
+            qoute: `${qouter}`,
+            newslink: `${linked}`,
+            source: `${newsSource}`,
+            newCaseArray,
+            newImageArray
+        }
+    })
+})
+
+app.get('/case', (req, res) => {
+
+    const caseIds = Object.keys(cases)      //takes an object and
+    const newCaseArray = caseIds.map( id => cases[id])      //converts it into an array
+    const newImageArray = caseIds.map( image => cases[image])
+
+    res.render('cases', {
+        locals: {       //makes an object
+            title: `${name}`,       //makes another object
+            qoute: `${qouter}`,
+            newslink: `${linked}`,
+            source: `${newsSource}`,
+            newCaseArray,
+            newImageArray
+        }
+    })
+})
+
+app.get("/case/:id", (req, res) => {
+    const caseFile = cases[req.params.id]
+
+    if(!caseFile){      //!caseFile === no caseFile
+        res.render('404')
+    }
+
+    res.render("caseid", {
+        locals: {
+            caseFile        //name of variable is the name of the key, i could write it like this
+        }
+    })
+})
+
+
+app.get("*", (req, res)=>{
+    res.render("404")
+})
+
+app.listen("3000", () => {
+    console.log('The server is running...')
+})
